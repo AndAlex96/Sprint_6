70dfe73cdc023962c17b5a0b9df27abc050b01a3
AndAlex96 <96plot@gmail.com>
2025-02-04 01:00:18 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,14 +8,16 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
+    @allure.step('Поиск элемента с заданным ожиданием')
     def find_element(self, locator):
         return WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(locator))
 
+    @allure.step('Нажатие на элемент')
     def click_on_element(self, locator):
         self.driver.find_element(*locator).click()
 
-# метод помещен в бэйс, т.к. для работы обязательно прямое обращение к driver
-    def assert_current_url_new_window(self, URL):
+    @allure.step('Получение текущего адреса в новом окне')
+    def get_current_url_new_window(self, URL):
         current_window = self.driver.current_window_handle
 
         for window_handle in self.driver.window_handles:
@@ -23,7 +25,8 @@ class BasePage:
                 self.driver.switch_to.window(window_handle)
                 break
         WebDriverWait(self.driver, 10).until(EC.url_to_be(URL))
-        assert self.driver.current_url == URL
+        return self.driver.current_url
 
-    def assert_current_url(self, URL):
-        assert self.driver.current_url == URL
+    @allure.step('Получение текущего адреса')
+    def get_current_url(self):
+        return self.driver.current_url
