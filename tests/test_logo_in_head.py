4253617f4909e427966d7ea88488457f947c187f
AndAlex96 <96plot@gmail.com>
2025-03-24 22:58:34 +0300

    Update test_logo_in_head.py

--- tests/test_logo_in_head.py
+++ tests/test_logo_in_head.py
@@ -2,7 +2,9 @@ from conftest import driver
 from pages.order_page import OrderPage
 from pages.start_page import StartPage
 import allure
-from start_page_locators import StartPageLocators
+import pytest
+
+from locators.start_page_locators import StartPageLocators
 
 
 class TestWorkLogoYaAndScooter:
@@ -12,7 +14,7 @@ class TestWorkLogoYaAndScooter:
         order_page = OrderPage(driver)
         start_page = StartPage(driver)
 
-        start_page.click_on_button_order(locator=StartPageLocators.upper_button_order)
+        start_page.click_on_button_order()
         order_page.filling_out_the_first_form('Андрей', 'Плотников', 'Королева 10', 'Черкизовская', '88008008080')
         order_page.click_on_next_button()
         order_page.filling_out_the_second_form()
@@ -29,13 +31,16 @@ class TestWorkLogoYaAndScooter:
         start_page = StartPage(driver)
         order_page = OrderPage(driver)
 
-        start_page.click_on_button_order(locator=StartPageLocators.upper_button_order)
+        start_page.click_on_button_order()
         order_page.filling_out_the_first_form('Андрей', 'Плотников', 'Королева 10', 'Черкизовская', '88008008080')
         order_page.click_on_next_button()
         order_page.filling_out_the_second_form()
         order_page.click_on_order_button()
         order_page.click_on_yes_button()
 
+        order_page.click_look_to_status_button()
+        order_page.click_on_logo_scooter_in_head()
+        order_page.assert_current_url(URL='https://qa-scooter.praktikum-services.ru/')
         order_page.click_look_to_status_button()
         order_page.click_on_logo_scooter_in_head()
         order_page.assert_current_url(URL='https://qa-scooter.praktikum-services.ru/')
